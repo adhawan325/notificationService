8b8a7e78bd943c6cc2e7623bf023965889e12257
adhawan325 <dhawan@gmail.com>
2017-05-19 21:27:23 -0700

    adding jersey resource

---
+++ src/main/java/org/s9/resources/MessageResource.java
@@ -0,0 +1,32 @@
+package org.s9.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.s9.domain.Message;
+
+@Path("people")
+public class MessageResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Message> getMessages() {
+
+      List<Message> messages = new ArrayList<Message>();
+
+      Message m1 = new Message("01", "Hello");
+      Message m2 = new Message("02", "I am here!");
+      Message m3 = new Message("03", "Where are you?");
+
+      messages.add(m1);
+      messages.add(m2);
+      messages.add(m3);
+
+      return messages;
+    }
+}
